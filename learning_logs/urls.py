b5ec110d7e7bf9bb0a816fa14579c9b5d271e77f
mingsec@outlook.com <nikit@qq.com>
2018-11-04 18:23:07 +0800

    准备部署到Heroku

---
+++ learning_logs/urls.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Author: ZhuZefeng
+# @Date:   2018-10-30 17:48:18
+# @Last Modified by:   ZhuZefeng
+# @Last Modified time: 2018-11-02 17:48:22
+
+
+"""
+定义learning_logs的URL模式
+"""
+
+from django.urls import path, re_path
+from .           import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+               #主页
+               path('',views.index, name='index'),
+
+               #显示所有主题
+               path('learning_logs/topics', views.topics, name='topics'),
+
+               #显示所有题目
+               re_path('learning_logs/(?P<topic_name>[A-Z]+)/entries', views.entries, name='entries'),
+
+               #显示所有条目
+               re_path('learning_logs/(?P<topic_name>[A-Z]+)/(?P<entry_name>[A-Z]+)/blogs', views.blogs, name='blogs'),
+
+               #用于添加新主题的页面 
+               path('learning_logs/new_topic', views.new_topic, name='new_topic'), 
+
+               #用于添加新科目的页面 
+               re_path('learning_logs/(?P<topic_name>[A-Z]+)/new_entry', views.new_entry, name='new_entry'),
+
+               #用于添加新条目的页面
+               re_path('learning_logs/(?P<topic_name>[A-Z]+)/(?P<entry_name>[A-Z]+)/new_blog', views.new_blog, name='new_blog'),
+
+               #用于编辑条目的页面
+               re_path('learning_logs/(?P<topic_name>[A-Z]+)/(?P<entry_name>[A-Z]+)/(?P<blog_name>[A-Z]+)/edit_blog', views.edit_blog, name='edit_blog'),
+
+
+              ]
